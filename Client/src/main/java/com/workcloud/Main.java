428dcf9e8fa4cff524de93896590ad0920cdaab3
MigSer13 <avto470@mail.ru>
2021-05-31 16:02:05 +0300

    trial-1

--- Client/src/main/java/com/workcloud/Main.java
+++ Client/src/main/java/com/workcloud/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/client.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("client.fxml"));
         primaryStage.setTitle("Storage");
         primaryStage.setScene(new Scene(root, 300, 400));
         primaryStage.show();
