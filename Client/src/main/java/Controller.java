0c36f79ced023ed850e2d1d5cb15d9c6e3de73e3
MigSer13 <avto470@mail.ru>
2021-06-06 13:54:26 +0300

    add button and action

--- Client/src/main/java/Controller.java
+++ Client/src/main/java/Controller.java
@@ -3,14 +3,63 @@ import com.workcloud.Client;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.input.ScrollEvent;
 
-public class Controller {
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+
+public class Controller implements Initializable {
     private Client client;
+    private String selectedPath = "E:/Example/";
+    private File fileinfo;
 
     @FXML
-    TextField fileName;
+    TextField textField;
+    @FXML
+    ListView<String> listView;
 
+    public void updateList() {
+        try {
+            listView.getItems().clear();
+            fileinfo = new File(selectedPath);
+            listView.getItems().addAll(fileinfo.list());
+            textField.setText(selectedPath);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        updateList();
+    }
+
+    public void mouseClicked(MouseEvent mouseEvent) {
+        if(mouseEvent.getClickCount() == 2) {
+            String selectedFile = listView.getSelectionModel().getSelectedItem();
+            selectedPath = selectedPath + selectedFile;
+            fileinfo = new File(selectedPath);
+            if(fileinfo.isDirectory()){
+                updateList();
+            }
+        }
+
+    }
+
+    public void backButtonClick(ActionEvent actionEvent) {
+
+    }
 }
